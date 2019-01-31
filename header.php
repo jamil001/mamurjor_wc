2af84c459ff715e8a72b007353d205fedb9a5727
jamil001 <47100044+jamil001@users.noreply.github.com>
2019-01-31 20:35:36 +0600

    Update header.php

--- header.php
+++ header.php
@@ -16,24 +16,23 @@
 	
 
 <head>
-		<!-- Meta -->
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-		<meta name="description" content="">
-		<meta name="author" content="">
-	    <meta name="keywords" content="MediaCenter, Template, eCommerce">
-	    <meta name="robots" content="all">
+	<!-- Meta -->
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="keywords" content="MediaCenter, Template, eCommerce">
+	<meta name="robots" content="all">
 
-	    
-
-
-
-        <?php wp_head(); ?>
+	
 
 
-	</head>
-    <body class="cnt-home" <?php body_class(); ?> > 
+
+	<?php wp_head(); ?>
+
+</head>
+<body class="cnt-home" <?php body_class(); ?> > 
 		<!-- ============================================== HEADER ============================================== -->
 <header class="header-style-1">
 
@@ -77,117 +76,93 @@
 	</div><!-- /.container -->
 </div><!-- /.header-top -->
 <!-- ============================================== TOP MENU : END ============================================== -->
-	<div class="main-header">
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-12 col-sm-12 col-md-3 logo-holder">
+<div class="main-header">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-3 logo-holder">
 					<!-- ============================================================= LOGO ============================================================= -->
-<div class="logo">
-	<a href=" <?php echo home_url( '/' ) ?>">
-		
-		
-		<img src=" <?php echo get_template_directory_uri().'/assets/images/logo.png' ; ?>" alt="">
+				<div class="logo">
+					<a href=" <?php echo home_url( '/' ) ?>">
+						
+						
+						<img src=" <?php echo get_template_directory_uri().'/assets/images/logo.png' ; ?>" alt="">
 
-	</a>
-</div><!-- /.logo -->
-<!-- ============================================================= LOGO : END ============================================================= -->				</div><!-- /.logo-holder -->
+					</a>
+				</div><!-- /.logo -->
+<!-- ============================================================= LOGO : END ============================================================= -->				
+			</div><!-- /.logo-holder -->
 
-				<div class="col-xs-12 col-sm-12 col-md-7 top-search-holder">
+			<div class="col-xs-12 col-sm-12 col-md-7 top-search-holder">
 					<!-- /.contact-row -->
 <!-- ============================================================= SEARCH AREA ============================================================= -->
-<div class="search-area">
-    <form>
-        <div class="control-group">
+				<div class="search-area">
+					<form>
+						<div class="control-group">
 
-            <ul class="categories-filter animate-dropdown">
-                <li class="dropdown">
+							<ul class="categories-filter animate-dropdown">
+								<li class="dropdown">
 
-                    <a class="dropdown-toggle"  data-toggle="dropdown" href="category.html">Categories <b class="caret"></b></a>
+									<a class="dropdown-toggle"  data-toggle="dropdown" href="category.html">Categories <b class="caret"></b></a>
 
-                    <ul class="dropdown-menu" role="menu" >
-                        <li class="menu-header">Computer</li>
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Clothing</a></li>
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Electronics</a></li>
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Shoes</a></li>
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Watches</a></li>
+									<ul class="dropdown-menu" role="menu" >
+										<li class="menu-header">Computer</li>
+										<li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Clothing</a></li>
+										<li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Electronics</a></li>
+										<li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Shoes</a></li>
+										<li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Watches</a></li>
 
-                    </ul>
-                </li>
-            </ul>
+									</ul>
+								</li>
+							</ul>
 
-            <input class="search-field" placeholder="Search here..." />
+							<input class="search-field" placeholder="Search here..." />
 
-            <a class="search-button" href="#" ></a>    
+							<a class="search-button" href="#" ></a>    
 
-        </div>
-    </form>
-</div><!-- /.search-area -->
-<!-- ============================================================= SEARCH AREA : END ============================================================= -->				</div><!-- /.top-search-holder -->
+						</div>
+					</form>
+				</div><!-- /.search-area -->
+<!-- ============================================================= SEARCH AREA : END ============================================================= -->				
+			</div><!-- /.top-search-holder -->
 
-				<div class="col-xs-12 col-sm-12 col-md-2 animate-dropdown top-cart-row">
+			<div class="col-xs-12 col-sm-12 col-md-2 animate-dropdown top-cart-row">
 					<!-- ============================================================= SHOPPING CART DROPDOWN ============================================================= -->
 
-	<div class="dropdown dropdown-cart">
-		<a href="#" class="dropdown-toggle lnk-cart" data-toggle="dropdown">
-			<div class="items-cart-inner">
-            <div class="basket">
-					<i class="glyphicon glyphicon-shopping-cart"></i>
-				</div>
-				<div class="basket-item-count"><span class="count">2</span></div>
-				<div class="total-price-basket">
-					<span class="lbl">cart -</span>
-					<span class="total-price">
-						<span class="sign">$</span><span class="value">600.00</span>
-					</span>
-				</div>
-				
-			
-		    </div>
-		</a>
-		<ul class="dropdown-menu">
-			<li>
-				<div class="cart-item product-summary">
-					<div class="row">
-						<div class="col-xs-4">
-							<div class="image">
-								<a href="detail.html"><img src="assets/images/cart.jpg" alt=""></a>
+				<div class="dropdown dropdown-cart">
+					
+					<?php global $woocommerce  ?>
+					
+					
+					<a href="<?php echo $woocommerce->cart->get_cart_url();  ?>" class="dropdown-toggle lnk-cart" data-toggle="dropdown">
+						<div class="items-cart-inner">
+							<div class="basket">
+								<i class="glyphicon glyphicon-shopping-cart"></i>
 							</div>
-						</div>
-						<div class="col-xs-7">
+							<div class="basket-item-count"><span class="count"><?php echo sprintf(_n('%d item','%d items',$woocommerce->cart->cart_content_count,'flipmart'),$woocommerce->cart->cart_content_count) ; ?></span></div>
+							<div class="total-price-basket">
+								<span class="lbl">cart -</span>
+								<span class="total-price">
+									<span class="sign">$</span><span class="value"><?php echo $woocommerce->cart->get_cart_total() ; ?></span>
+								</span>
+							</div>
 							
-							<h3 class="name"><a href="index8a95.html?page-detail">Simple Product</a></h3>
-							<div class="price">$600.00</div>
-						</div>
-						<div class="col-xs-1 action">
-							<a href="#"><i class="fa fa-trash"></i></a>
-						</div>
-					</div>
-				</div><!-- /.cart-item -->
-				<div class="clearfix"></div>
-			<hr>
-		
-			<div class="clearfix cart-total">
-				<div class="pull-right">
-					
-						<span class="text">Sub Total :</span><span class='price'>$600.00</span>
 						
-				</div>
-				<div class="clearfix"></div>
+						</div>
+					</a>
 					
-				<a href="checkout.html" class="btn btn-upper btn-primary btn-block m-t-20">Checkout</a>	
-			</div><!-- /.cart-total-->
 					
-				
-		</li>
-		</ul><!-- /.dropdown-menu-->
-	</div><!-- /.dropdown-cart -->
+					
+					
+					
+				</div><!-- /.dropdown-cart -->
 
-<!-- ============================================================= SHOPPING CART DROPDOWN : END============================================================= -->				</div><!-- /.top-cart-row -->
-			</div><!-- /.row -->
+			<!-- ============================================================= SHOPPING CART DROPDOWN : END============================================================= -->				
+			</div><!-- /.top-cart-row -->
+		</div><!-- /.row -->
 
-		</div><!-- /.container -->
+	</div><!-- /.container -->
 
-	</div><!-- /.main-header -->
+</div><!-- /.main-header -->
 
 	<!-- ============================================== NAVBAR ============================================== -->
 <div class="header-nav animate-dropdown">
@@ -203,22 +178,22 @@
             </div>
             <div class="nav-bg-class">
                 <div class="navbar-collapse collapse" id="mc-horizontal-menu-collapse">
-	<div class="nav-outer">
-		
-		<?php 
-		wp_nav_menu( array(
-			'theme_location'  => 'main_menu',
-			'container_class' => 'nav-outer',
-			'menu_class'      => 'nav navbar-nav',
-			
-		) );
-		 ?>
+					<div class="nav-outer">
+						
+						<?php 
+						wp_nav_menu( array(
+							'theme_location'  => 'main_menu',
+							'container_class' => 'nav-outer',
+							'menu_class'      => 'nav navbar-nav',
+							
+						) );
+						 ?>
 
 
-	
-		<div class="clearfix"></div>				
-	</div><!-- /.nav-outer -->
-</div><!-- /.navbar-collapse -->
+					
+						<div class="clearfix"></div>				
+					</div><!-- /.nav-outer -->
+				</div><!-- /.navbar-collapse -->
 
 
             </div><!-- /.nav-bg-class -->
