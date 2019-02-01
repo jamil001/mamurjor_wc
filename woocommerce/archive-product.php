73cf95c5fa834a48fe2f6b02341c908525355545
jamil001 <47100044+jamil001@users.noreply.github.com>
2019-02-01 23:54:58 +0600

    Update archive-product.php

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -812,6 +812,9 @@ if ( woocommerce_product_loop() ) {
  * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
  */
 do_action( 'woocommerce_after_main_content' );
+		    
+		    
+		    
 
 /**
  * Hook: woocommerce_sidebar.
